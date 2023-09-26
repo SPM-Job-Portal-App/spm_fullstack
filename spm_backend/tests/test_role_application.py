6cfb1bff9df0eebf8f8b6c1403247cdd19ec4d19
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-09-26 10:44:24 +0800

    added test _role

--- spm_backend/tests/test_role_application.py
+++ spm_backend/tests/test_role_application.py
@@ -9,7 +9,3 @@ def client():
     client = app.test_client()
     yield client
 
-def test_apply_for_open_role_listing(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.data == b"Flask API works!"
