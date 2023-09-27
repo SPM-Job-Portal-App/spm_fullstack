cc045cce96cfd2eb8c50f36221cc5d689cada9c3
Aloysius Tan <69228664+narnia1601@users.noreply.github.com>
2023-09-27 14:46:54 +0800

    edited main.py

--- spm_backend/main.py
+++ spm_backend/main.py
@@ -16,18 +16,15 @@ def initialize_databases():
     with app.app_context():
         db.create_all()
 
-# Try connecting to MySQL on localhost first
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://user:password@localhost:3306/db'
-
-# If unable to connect to localhost, fall back to the "mysql" service defined in Docker Compose
 try:
+    # Try connecting to MySQL on localhost first
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://user:password@localhost:3306/db'
     db.init_app(app)
     app.register_blueprint(listing_bp, url_prefix='/listing')
     app.register_blueprint(application_bp, url_prefix='/application')
     app.register_blueprint(role_bp, url_prefix='/role')
 except Exception as e:
     print(f"Failed to connect to MySQL on localhost: {e}")
-    
     # Try connecting to the "mysql" service defined in Docker Compose
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@mysql:3306/db'
     db.init_app(app)
