29b4014dcdd816b013e43fac21ff3fa47e7bec6d
CCJH23 <clovischowjh@gmail.com>
2023-10-03 21:55:19 +0800

    Added role_skill model, sql, controller & service

---
+++ spm_backend/role_skill/roleSkillController.py
@@ -0,0 +1,13 @@
+from flask import jsonify, Blueprint
+from role_skill.roleSkillService import RoleSkillService
+role_skill_bp = Blueprint('role_skill', __name__)
+
+# Get role skills by role_name
+@role_skill_bp.route('/get_skills_by_role_name/<role_name>')
+def get_skills_by_role_name(role_name):
+    try:
+        response = RoleSkillService.get_skills_by_role_name(role_name)
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
