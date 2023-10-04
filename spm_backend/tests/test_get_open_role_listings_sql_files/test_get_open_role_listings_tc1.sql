4161815e9ce17c30a7658ee29fbac3afaeb809a6
CCJH23 <clovischowjh@gmail.com>
2023-10-04 14:56:07 +0800

    Updated sql file for opening and closing date

--- spm_backend/tests/test_get_open_role_listings_sql_files/test_get_open_role_listings_tc1.sql
+++ spm_backend/tests/test_get_open_role_listings_sql_files/test_get_open_role_listings_tc1.sql
@@ -57,13 +57,15 @@ CREATE TABLE role_listing (
     country VARCHAR(255) NOT NULL,
     dept VARCHAR(255) NOT NULL,
     is_open BOOLEAN NOT NULL,
+    opening_date DATE,
+    closing_date DATE,
     reporting_manager INT,
     FOREIGN KEY (role_name) REFERENCES role (role_name) ON DELETE CASCADE,
     FOREIGN KEY (reporting_manager) REFERENCES staff(id) ON DELETE CASCADE
 );
 
 -- Insert data into the role_listing table
-INSERT INTO role_listing (role_name, country, dept, is_open, reporting_manager)
+INSERT INTO role_listing (role_name, country, dept, is_open, opening_date, closing_date, reporting_manager)
 VALUES
-    ('Developer', 'Canada', 'IT', True, 1);
+    ('Developer', 'Canada', 'IT', True, '2023-10-01', '2023-10-15', 1);
     
