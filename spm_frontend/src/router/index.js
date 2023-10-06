8b7421e6854d105af771b602617a921942c05068
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-06 16:00:41 +0800

    changed the role.csv to solve the encoding issue

---
+++ spm_frontend/src/router/index.js
@@ -0,0 +1,90 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+import DefaultLayout from '@/layouts/default/Default.vue'
+import Home from '@/views/Home.vue'
+import OpenRoles from '@/views/OpenRoles.vue'
+import ApplyOpenRoles from '@/views/ApplyOpenRoles.vue'
+import Candidates from '@/views/Candidates.vue'
+import Settings from '@/views/Settings.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: '/openroles/staff',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Open Roles Staff',
+        component: () => import('@/views/OpenRoles_Staff.vue'),
+      },
+    ],
+  },
+  {
+    path: '/openroles/hr',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Open Roles HR',
+        component: () => import('@/views/OpenRoles_HR.vue'),
+      },
+    ],
+  },
+  {
+    path: '/candidates',
+    component: DefaultLayout,
+    children: [
+      {
+        path: '',
+        name: 'Candidates',
+        component: Candidates,
+      },
+    ],
+  },
+  {
+    path: '/settings',
+    component: DefaultLayout,
+    children: [
+      {
+        path: '',
+        name: 'Settings',
+        component: Settings,
+      },
+    ],
+  },
+  {
+    path: '/createrolelisting',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Create Role Listing',
+        component: () => import('@/views/CreateRoleListing.vue'),
+      },
+    ],
+  },
+  {
+    path: '/edit-listing/:index', // Define a route parameter :index
+    name: 'edit-listing',
+    component: () => import('@/components/EditListing.vue'),
+    props: true,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
