83079cf077fc5dbe3b8f24e66c84ed5af055be24
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-09 19:23:34 +0800

    Finished with importing staff and access log

--- spm_backend/models/staff_model.py
+++ spm_backend/models/staff_model.py
@@ -7,12 +7,22 @@ class Staff(db.Model):
     dept = db.Column(db.String(255), nullable=False)
     country = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    role = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, staff_first_name, staff_last_name, dept, country, email, role):
+    # Define a foreign key relationship to AccessControl
+    role = db.Column(db.Integer, db.ForeignKey('access_control.access_id'), nullable=False)
+
+    # Define a relationship with the AccessControl model
+    access_control = db.relationship('AccessControl', backref='staff', foreign_keys=[role])
+
+    def __init__(self, id,staff_first_name, staff_last_name, dept, country, email, role):
+        self.id =id
         self.staff_first_name = staff_first_name
         self.staff_last_name = staff_last_name
         self.dept = dept
         self.country = country
         self.email = email
         self.role = role
+
+    def json(self):
+        return {"Staff Name": self.staff_first_name + " " + self.staff_last_name, "Department": self.dept, "Country": self.country, "Email": self.email, "Role": self.role}
+    
--- spm_backend/staff/staff.sql
+++ spm_backend/staff/staff.sql
@@ -1,4 +1,3 @@
--- Create the staff table
 CREATE TABLE staff (
     id INT AUTO_INCREMENT PRIMARY KEY,
     staff_first_name VARCHAR(255) NOT NULL,
@@ -6,12 +5,7 @@ CREATE TABLE staff (
     dept VARCHAR(255) NOT NULL,
     country VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
+    `role` INT NOT NULL,  -- Change data type to INT to match access_id
+    FOREIGN KEY (`role`) REFERENCES access_control (access_id) ON DELETE CASCADE
 );
 
--- Insert data into the staff table
-INSERT INTO staff (staff_first_name, staff_last_name, dept, country, email, role)
-VALUES
-    ('John', 'Doe', 'Sales', 'USA', 'john.doe@example.com', 'Manager'),
-    ('Alice', 'Smith', 'Engineering', 'Canada', 'alice.smith@example.com', 'Developer'),
-    ('Emma', 'Wilson', 'Solutioning', 'UK', 'emma.wilson@example.com', 'Designer');
