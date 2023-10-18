aadcdfcc217cb4bc164018229cd4ca3af94e1e05
joelohkaishunhiroyuki <joel.oh.2021@smu.edu.sg>
2023-10-18 11:48:57 +0800

    edit role listing test case has failed due to failure of updating child row

--- spm_backend/models/role_listing_model.py
+++ spm_backend/models/role_listing_model.py
@@ -1,15 +1,23 @@
 from models.model import db
 
 class RoleListing(db.Model):
+    # __tablename__ = 'rolelisting'
     id = db.Column(db.Integer, primary_key=True)
-    role_name = db.Column(db.String(255), nullable=False)
+    # role_name = db.Column(db.String(255), nullable=False)
     country = db.Column(db.String(255), nullable=False)
     dept = db.Column(db.String(255), nullable=False)
     is_open = db.Column(db.Boolean, nullable=False)
     opening_date = db.Column(db.Date, nullable=False)
     closing_date = db.Column(db.Date, nullable=False)
-    reporting_manager = db.Column(db.Integer, nullable=True)
-
+    # reporting_manager = db.Column(db.Integer, nullable=True)
+    
+    # Define foreign key
+    role_name = db.Column(db.String(255), db.ForeignKey('role.role_name'), nullable=False)
+    reporting_manager = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=True)
+    
+    # relationships
+    # var = db.relationship('Role', foreign_keys=[role_name], cascade="save-update, merge")
+    # var2 = db.relationship('Role', foreign_keys=[reporting_manager], cascade="save-update, merge")
 
     def __init__(self, role_name, country, dept, is_open, opening_date, closing_date, reporting_manager):
         self.role_name = role_name
